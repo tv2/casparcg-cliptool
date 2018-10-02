ca6e73416b9f3442942b5fb82fcf6ecb1588077f
olzzon <github@olzzon.dk>
2018-10-02 17:27:33 +0200

    Changed Appname - may break yealier settings Fix: Name of app window: added title: "CasparCG ClipTool" in webpack.build.config.js

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -38,7 +38,9 @@ module.exports = {
   },
   target: 'electron-renderer',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'CasparCG ClipTool'
+    }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
