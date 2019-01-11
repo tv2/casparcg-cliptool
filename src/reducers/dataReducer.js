0aa6b659a1ec9bfa6ed52d4d796200b6440621b0
olzzon <github@olzzon.dk>
2019-01-11 17:50:44 +0100

    Fixed: Counter - hours

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -21,7 +21,7 @@ const FPS = 25;
 
 const secondsToTimeCode = (time => {
     if (time) {
-        var hour = ('0' + (time/(60*60)).toFixed()).slice(-2);
+        var hour = ('0' + parseInt((time/60)*60)).slice(-2);
         var minute = ('0' + parseInt(time/(60))).slice(-2);
         var sec = ('0' + (parseInt(time % 60))).slice(-2);
         var frm = ('0' + (100*(time - parseInt(time))*(FPS/100)).toFixed()).slice(-2);
