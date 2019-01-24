56f736376b8d907273c332c40bf854f56bc50788
olzzon <github@olzzon.dk>
2019-01-24 20:49:30 +0100

    Fix: Avoid calling an overlay template or the wipe 2 frames in a row.

--- src/util/HandleOverlay.js
+++ src/util/HandleOverlay.js
@@ -5,6 +5,9 @@ class HandleOverlay {
         const unsubscribe = store.subscribe(() => {
             this.store = window.store.getState();
         });
+
+        this.overlayIsStarted = [false, false, false, false];
+        this.wipeIsStarted = [false, false, false, false];
     }
 
     overlay(item, indexChannel) {
@@ -12,20 +15,12 @@ class HandleOverlay {
         const metaData = this.store.dataReducer[0].data.channel[indexChannel].thumbList[thumbIndex].metaList;
         const overlayFolder = this.store.settingsReducer[0].settings.tabData[indexChannel].overlayFolder;
 
-        if (item.time < 0.14) {
-            console.log(
-                ' Index : ', this.store.dataReducer[0].data.channel[indexChannel].thumbActiveIndex,
-                ' Paused : ', this.store.dataReducer[0].data.ccgInfo[indexChannel].layers[9].foreground.paused,
-                ' Time :', item.time
-            );
-        }
-
         if (overlayFolder != '' && !this.store.dataReducer[0].data.ccgInfo[indexChannel].layers[9].foreground.paused) {
             metaData.map((metaItem) => {
                 if (metaItem.startTime < 0.08) {
                     metaItem.startTime = 0.08;
                 }
-                if (metaItem.startTime < item.time && item.time < (metaItem.startTime + 0.09)) {
+                if (metaItem.startTime < item.time && item.time < (metaItem.startTime + 0.0799) && !this.overlayIsStarted) {
                     console.log("Lower third on: ", metaItem.startTime, item.time, metaItem.templateData[0].data);
                     this.ccgConnection.cgAdd(
                         1,20, 1,
@@ -33,18 +28,28 @@ class HandleOverlay {
                         1,
                         this.metaDataToXml(metaItem)
                     );
-                }
-                if ((metaItem.startTime + metaItem.duration) < item.time && item.time < (metaItem.startTime + metaItem.duration + 0.08)) {
+                    this.overlayIsStarted = true;
+                } else if ((metaItem.startTime + metaItem.duration) < item.time && item.time < (metaItem.startTime + metaItem.duration + 0.0799) && !this.overlayIsStarted) {
                     console.log("Lower third OFF: ", (metaItem.startTime + metaItem.duration), item.time, metaItem.templateData[0].data);
                     this.ccgConnection.clear(indexChannel + 1, 20);
+                    this.overlayIsStarted = true;
+                } else {
+                    this.overlayIsStarted = false;
                 }
 
-                //ToDo: only fire once.
-                let wipeOffset = parseFloat(this.store.settingsReducer[0].settings.tabData[indexChannel].wipeOffset);
-                if (wipeOffset < item.timeLeft && item.timeLeft < (wipeOffset + 0.5) && this.store.settingsReducer[0].settings.tabData[indexChannel].autoPlay) {
-                    this.ccgConnection.play(1, 11, this.store.settingsReducer[0].settings.tabData[indexChannel].wipe);
-                }
             });
+        }
+        //ToDo: only fire once.
+        if (this.store.settingsReducer[0].settings.tabData[indexChannel].wipe != '' &&
+            this.store.settingsReducer[0].settings.tabData[indexChannel].autoPlay
+        ) {
+            let wipeOffset = parseFloat(this.store.settingsReducer[0].settings.tabData[indexChannel].wipeOffset);
+            if (wipeOffset < item.timeLeft && item.timeLeft < (wipeOffset + 0.0799) && !this.wipeIsStarted) {
+                this.ccgConnection.play(1, 11, this.store.settingsReducer[0].settings.tabData[indexChannel].wipe);
+                this.wipeIsStarted = true;
+            } else {
+                this.wipeIsStarted = false;
+            }
         }
     }
 
