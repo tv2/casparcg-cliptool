e4cf798583263e11f264ff91775dee1fc162a8af
olzzon <github@olzzon.dk>
2021-12-09 08:40:42 +0100

    feat: Different label (SELECTED or END) depended on whether it's a video clip or a still pix

--- src/client/util/TimeCodeToString.ts
+++ src/client/util/TimeCodeToString.ts
@@ -1,20 +1,23 @@
 const FPS = 25
 
 export const secondsToTimeCode = (timer: [number, number] = [0, 0]) => {
-    let time = timer[1] - timer[0]
-    if (time <= 0) {
-        time = 0
+    if (timer[1] > 0) {
+        let time = timer[1] - timer[0]
+        if (time <= 0) {
+            time = 0
+        }
+        if (time) {
+            var hour = ('0' + Math.round(time / (60 * 60))).slice(-2)
+            var minute = ('0' + Math.round(time / 60)).slice(-2)
+            var sec = ('0' + Math.round(time % 60)).slice(-2)
+            var frm = (
+                '0' +
+                (100 * (time - parseInt(String(time))) * (FPS / 100)).toFixed()
+            ).slice(-2)
+            return ' ' + hour + '.' + minute + '.' + sec + '.' + frm + ' '
+        } else {
+            return '****END****'
+        }
     }
-    if (time) {
-        var hour = ('0' + Math.round(time / (60 * 60))).slice(-2)
-        var minute = ('0' + Math.round(time / 60)).slice(-2)
-        var sec = ('0' + Math.round(time % 60)).slice(-2)
-        var frm = (
-            '0' +
-            (100 * (time - parseInt(String(time))) * (FPS / 100)).toFixed()
-        ).slice(-2)
-        return ' ' + hour + '.' + minute + '.' + sec + '.' + frm + ' '
-    } else {
-        return ' SELECTED '
-    }
+    return 'SELECTED'
 }
