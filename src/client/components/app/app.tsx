20afcc7954b15f3b0877b9d2abea0c90f92a76a2
andr9528 <andre@steenhoff.dk>
2023-04-24 12:18:18 +0200

    UT-210: refactor: changes usage of jsx-service into a new component instead, called component-decider.

--- src/client/components/app/app.tsx
+++ src/client/components/app/app.tsx
@@ -2,21 +2,14 @@ import React from 'react'
 import { reduxStore } from '../../../model/reducers/store'
 import { setActiveTab } from '../../../model/reducers/app-navigation-action'
 import { OperationModeFooter } from '../footer/operation-mode-footer'
-import { useSelector } from 'react-redux'
-import { Settings } from '../settings/settings/settings'
 import Header from '../header/header/header'
-import { State } from '../../../model/reducers/index-reducer'
-import Main from '../main'
-import OfflineOverlay from '../offline-overlay/offline-overlay'
-import jsxService from '../../services/jsx-service'
 import browserService from '../../services/browser-service'
 import './app.scss'
+import ComponentDecider from '../component-decider'
 
 
 export function App(): JSX.Element {
-    const isSettingsOpen = useSelector((state: State) => state.appNavigation.isSettingsOpen)
-    const isConnected: boolean = useSelector(
-        (state: State) => state.appNavigation.isConnected)
+
     if (browserService.isChannelView()) {
         setOutput(browserService.getChannel() - 1)
     }   
@@ -25,15 +18,7 @@ export function App(): JSX.Element {
         <div className="app">
             <Header />
             <div className="app-body">
-                {
-                    jsxService.decideJsx(
-                        !isConnected, 
-                        <OfflineOverlay />, 
-                        jsxService.decideJsx(
-                            isSettingsOpen, 
-                            <Settings />, 
-                            <Main />)) 
-                }
+                <ComponentDecider/>
             </div>
             <OperationModeFooter />
         </div>
---
+++ src/client/components/component-decider.tsx
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux"
+import { State } from "../../model/reducers/index-reducer"
+import OfflineOverlay from "./offline-overlay/offline-overlay"
+import React from "react"
+import { Settings } from "./settings/settings/settings"
+import Main from "./main"
+
+export default function ComponentDecider() {    
+    const isConnected: boolean = useSelector(
+        (state: State) => state.appNavigation.isConnected)
+    const isSettingsOpen = useSelector((state: State) => state.appNavigation.isSettingsOpen)
+
+    if (!isConnected) {
+      return (<OfflineOverlay />)
+    }
+    if (isSettingsOpen) {
+      return (<Settings />)
+    }
+    return (<Main />)
+}
